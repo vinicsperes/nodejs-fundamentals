4b1b190c1bab11fa0c6b811817211539bdd0a1e4
ohperes <peresvini2@gmail.com>
2023-01-16 17:00:14 -0300

    create a server to make some requests

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import http, { createServer } from 'node:http'
+
+const users = [];
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
+  console.log(req.body)
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const {name, email } = req.body
+
+    users.push({
+      id: 1,
+      name,
+      email,
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end('Not found')
+})
+
+server.listen(3333)
