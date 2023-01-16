27ee785272e025801124e333cb073226fb8413f3
ohperes <peresvini2@gmail.com>
2023-01-16 18:42:14 -0300

    separate routes from the server

--- src/server.js
+++ src/server.js
@@ -1,28 +1,20 @@
 import http, { createServer } from 'node:http'
 import { json } from './middlewares/json.js'
+import { routes } from './middlewares/routes.js'
 
-const users = [];
+
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
-  }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'POST' && url === '/users') {
-    const {name, email } = req.body
-
-    users.push({
-      id: 1,
-      name,
-      email,
-    })
-
-    return res.writeHead(201).end()
+  if (rout) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end('Not found')
