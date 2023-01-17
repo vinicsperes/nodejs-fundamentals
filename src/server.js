e50e30ccd6d02fbd963ff4131f20a89d2da04f82
ohperes <peresvini2@gmail.com>
2023-01-17 16:15:59 -0300

    catch query params

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http, { createServer } from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from './middlewares/routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 
 
@@ -16,7 +17,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path)
 
-    req.params = { ...routeParams.groups }
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
     return route.handler(req, res)
   }
